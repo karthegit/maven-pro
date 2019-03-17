cae3a1639bcc04118b46d6617e47f0555441ea4b
Kartheeswaran <karthe.heaven@yahoo.com>
2019-03-17 12:06:21 +0530

    my maven project

---
+++ karthe/src/main/java/karthe/app/App.java
@@ -0,0 +1,13 @@
+package karthe.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
